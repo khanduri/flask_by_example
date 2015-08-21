afe5ba19f4208153d01ed69a62dd0544b9656c0d
Prashant Khanduri <prashant.khanduri@gmail.com>
2015-08-21 13:39:43 -0700

    New app method to test on stage first

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello %s" % name
+
 
 if __name__ == '__main__':
     app.run()
